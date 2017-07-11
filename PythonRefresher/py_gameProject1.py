fb2f07dc2dbaffcb3fe9cff80f93144b01d546fa
ramtinj <ramtinj@kth.se>
2017-07-11 18:06:55 +0200

    most of the class code done

--- PythonRefresher/py_gameProject1.py
+++ PythonRefresher/py_gameProject1.py
@@ -3,32 +3,65 @@ from random import shuffle
 SUITE = 'H D S C'.split()
 RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
 
-mycards = [(s,r) for s in SUITE for r in RANKS]
-
 class Deck:
     """
     This is the Deck Class. This object will create a deck of cards to initiate
-    play. You can then use this Deck list of cards to split in half and give to
-    the players. It will use SUITE and RANKS to create the deck. It should also
-    have a method for splitting/cutting the deck in half and Shuffling the deck.
+    play. 
     """
     def __init__(self):
         print("Creating new deck")
-        self.allcards = 
+        self.allcards = [(s,r) for s in SUITE for r in RANKS]
+
+    def shuffle(self):
+        print("Shuffling deck")
+        shuffle(self.allcards)
+
+    def split_in_half(self):
+        return(self.allcards[:26], self.allcards[26:])
 
 class Hand:
     '''
     This is the Hand class. Each player has a Hand, and can add or remove
-    cards from that hand. There should be an add and remove card method here.
+    cards from that hand. 
     '''
-    pass
+    def __init__(self,cards):
+        self.cards = cards
+
+    def __str__(self):
+        return "Contains {} cards".format(len(self.cards))
+
+    def add(self,added_cards):
+        self.cards.extend(added_cards)
+
+    def remove_card(self):
+        return self.cards.pop()
 
 class Player:
     """
     This is the Player class, which takes in a name and an instance of a Hand
     class object. The Payer can then play cards and check if they still have cards.
     """
-    pass
+    def __init__(self,name,hand):
+        self.name = name
+        self.hand = hand
+
+    def play_card(self):
+        drawn_card = self.hand.remove_card()
+        print("{} has placed: {}".format(self.name, drawn_card))
+        print("\n")
+        return drawn_card
+
+    def remove_war_cards(self):
+        war_cards = []
+        for x in range(3):
+            war_cards.append(self.hand.cards.pop())
+        return war_cards
+
+    def still_has_cards(self):
+        """
+        True if player still has cards left
+        """
+        return len(self.hand.cards) != 0
 
 
 ######################
@@ -36,4 +69,3 @@ class Player:
 ######################
 print("Welcome to War, let's begin...")
 
-# Use the 3 classes along with some logic to play a game of war!
